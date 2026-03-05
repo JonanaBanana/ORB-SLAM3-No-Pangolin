931b2444c3c9b56555bbf67bae346704577f2cb2
Jonathan Nielsen <jonathan@majj.dk>
2026-03-05 11:03:13 +0100

    updated for c++17 support

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release
+ninja
 
 cd ../../g2o
 
@@ -12,8 +12,8 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release
+ninja
 
 cd ../../Sophus
 
@@ -21,9 +21,9 @@ echo "Configuring and building Thirdparty/Sophus ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
-sudo make install
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release
+ninja
+sudo ninja install
 
 cd ../../../
 
@@ -37,5 +37,5 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
-cmake ..
-make -j4
+cmake .. -GNinja
+ninja
